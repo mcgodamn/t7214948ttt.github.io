cb05933b8d324e7bc97b87dbf5810229577f8895
MJChung <t7214948ttt@gmail.com>
2019-11-14 11:29:02 +0800

    delete node

--- SweetyWeb/gulpfile.js
+++
@@ -1,8 +0,0 @@
-let gulp = require("gulp");
-let uglify = require('gulp-uglify-es').default;
- 
-gulp.task("uglify", function () {
-    return gulp.src('Scripts/*.js')
-        .pipe(uglify())
-        .pipe(gulp.dest('Scripts/minify'));
-});
